433da07b652bc5b0ab5a6e5f77bddfffa8094dcc
JamsJam <jamsjam971@hotmail.com>
2024-11-26 20:32:37 -0400

    CTA and hebergement bandeau

--- src/Twig/Components/CtaBan.php
+++ src/Twig/Components/CtaBan.php
@@ -7,5 +7,7 @@ use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
 #[AsTwigComponent]
 final class CtaBan
 {
-    
+    public string $titre;
+    public string $text;
+    public string $ctatitre;
 }
