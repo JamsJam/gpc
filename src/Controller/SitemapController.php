8ebd4bd76b0b05cbc8a57e9fe3deac13801230e1
JamsJam <jamsjam971@hotmail.com>
2024-12-12 18:19:12 -0400

    add sitemap

---
+++ src/Controller/SitemapController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SitemapController extends AbstractController
+{
+
+    #[Route(path:'/sitemap.xml', name:'app_sitemap' )]
+    public function index(Request $request)
+    {
+        // Nous récupérons le nom d'hôte depuis l'URL
+        $hostname = $request->getSchemeAndHttpHost();
+
+        // On initialise un tableau pour lister les URLs
+        $urls = [];
+
+        // On ajoute les URLs "statiques"
+        $urls[] = ['loc' => $this->generateUrl('website.index')];
+
+        // Fabrication de la réponse XML
+        $response = new Response(
+            $this->renderView('sitemap/index.html.twig', [
+                    'urls' => $urls,
+                    'hostname' => $hostname]
+            )
+        );
+
+        // Ajout des entêtes
+        $response->headers->set('Content-Type', 'text/xml');
+
+        // On envoie la réponse
+        return $response;
+    }
+}
