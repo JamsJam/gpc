fff483376f06d33b4c796b4497043aed6f7fa018
JamsJam <jamsjam971@hotmail.com>
2025-01-15 04:00:38 -0400

    full translation static content

--- src/Controller/AboutController.php
+++ src/Controller/AboutController.php
@@ -2,19 +2,27 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Trait\LocaleTrait;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AboutController extends AbstractController
 {
+    use LocaleTrait;
+    
     #[Route('/apropos', name: 'app_about')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        
+        $locale = $request->getLocale();
+        $redirect = $this->handleLocale($request);
+        if($redirect){
+            return $redirect;
+        }
 
         return $this->render('about/index.html.twig', [
-            'controller_name' => 'AboutController',
+            'locale' => $locale,
         ]);
     }
 }
--- src/Controller/CatalogueController.php
+++ src/Controller/CatalogueController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use Exception;
+use App\Trait\LocaleTrait;
 use App\Repository\ActivitesRepository;
 use App\Repository\ExcursionsRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -12,9 +14,16 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class CatalogueController extends AbstractController
 {
+    use LocaleTrait ;
+
     #[Route('/catalogue/{type}', name: 'app_catalogue')]
-    public function index(string $type, ActivitesRepository $ar,ExcursionsRepository $er): Response
+    public function index(Request $request, string $type, ActivitesRepository $ar,ExcursionsRepository $er): Response
     {
+        $locale = $request->getLocale();
+        $redirect = $this->handleLocale($request);
+        if($redirect){
+            return $redirect;
+        }
         $carteCollection = match ($type) {
             'activites' => $ar->findAll(),
             'excursions' => $er->findAll(),
@@ -26,7 +35,8 @@ class CatalogueController extends AbstractController
         return $this->render('catalogue/index.html.twig', [
             'collection' => $carteCollection,
             'type' => $type,
-            'modalOnPage' => $modalOnPage
+            'modalOnPage' => $modalOnPage,
+            "locale" => $locale
         ]);
     }
 }
